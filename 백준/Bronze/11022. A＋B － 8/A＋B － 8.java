24b43344e0764dadf53de867c9184afe0d0febd6
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-07-25 19:08:55 +0900

    [Bronze V] Title: A+B - 8, Time: 280 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		int cnt = 0 ;
+		
+		for(int i=1; i<=T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int sum = A+B;
+			cnt += 1;
+			System.out.println("Case #"+cnt+": "+A+" + "+B+" = "+sum);
+			}
+	}
+
+}
