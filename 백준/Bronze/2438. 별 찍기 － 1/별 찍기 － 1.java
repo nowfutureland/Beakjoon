fbc659ac97af75e337ad692b28987a8c3f9e3032
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-08-03 23:33:16 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 296 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
-		int N = sc.nextInt();
+		int N = scan.nextInt();
 		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=i; j++) {
+		for(int i=1; i<N+1; i++) {		 
+			for(int j=1; j < i+1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
