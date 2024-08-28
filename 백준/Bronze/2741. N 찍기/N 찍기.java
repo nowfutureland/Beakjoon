e15cc56288bbf2d67432fbb2a33ef7551ad42e9e
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-08-28 23:39:31 +0900

    [Bronze V] Title: N 찍기, Time: 808 ms, Memory: 32324 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
-		long N = sc.nextLong();
+		int A = scan.nextInt();
 		
-		for(int i=1; i<=N; i++) {
+		for(int i=1; i<=A; i++) {
 			System.out.println(i);
 		}
-		
 	}
+
 }
