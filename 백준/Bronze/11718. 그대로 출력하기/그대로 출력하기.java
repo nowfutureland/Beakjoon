5fcdb9582337899e7a1c0ddcff5d5ed005b136b0
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-08-30 22:36:04 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String A = sc.nextLine();
+			
+			System.out.println(A);
+		}
+		sc.close();
+	}
+}
